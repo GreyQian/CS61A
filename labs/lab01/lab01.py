02db9273b0c5311ed14db194c3adebd409bda83d
GreyQian <greyqian@qq.com>
2024-03-10 00:54:43 -0800

    finish ch1.3,1.4,1.5 and lab01

---
+++ labs/lab01/lab01.py
@@ -0,0 +1,101 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+    answer = 1
+    while k:
+        answer *= n
+        n, k = n - 1, k - 1
+    return answer
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    step, div = 0, k
+    while n >= div:
+        print(div)
+        step, div = step + 1, div + k
+    return step
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum_y, remainder = 0, 0
+    while y:
+        y, remainder = y // 10, y % 10
+        sum_y += remainder
+    return sum_y
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    while n:
+        n, remainder = n // 10, n % 100
+        if remainder == 88:
+            return True
+    return False
+    # while n:
+    #     n, remainder = n // 10, n % 10
+    #     if remainder == 8:
+    #         sencond_remainder = n % 10
+    #         if sencond_remainder == 8:
+    #             return True
+    #         else:
+    #             n = n // 10
+    # return False
